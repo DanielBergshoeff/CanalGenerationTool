2834d79eef42815068715a14749d917174357b46
DanielBergshoeff <dabergshoeff@gmail.com>
2021-05-27 20:57:31 +0200

    Initial commit

---
+++ Assets/Editor/HouseEditorEditor.cs
@@ -0,0 +1,369 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(HouseEditor))]
+public class HouseEditorEditor : Editor
+{
+    private ChoiceSelection currentChoiceSelection;
+    private WindowChoiceSelection currentWindowChoice;
+
+    private List<KeyCode> keyCodes = new List<KeyCode> { KeyCode.Keypad0, KeyCode.Keypad1, KeyCode.Keypad2, KeyCode.Keypad3, KeyCode.Keypad4, KeyCode.Keypad5, KeyCode.Keypad6, KeyCode.Keypad7, KeyCode.Keypad8, KeyCode.Keypad9 };
+
+    private void OnSceneGUI() {
+        HouseEditor h = target as HouseEditor;
+
+        if (h == null || h.gameObject == null)
+            return;
+
+        SetSelection(h);
+
+        Event e = Event.current;
+        switch (e.type) {
+            case EventType.KeyDown:
+                if (e.keyCode == KeyCode.U)
+                    Add(h.CurrentSelection, h);
+                else if (e.keyCode == KeyCode.I)
+                    Reduce(h.CurrentSelection, h);
+                else if (e.keyCode == KeyCode.C)
+                    SetChoiceSelection(ChoiceSelection.Color);
+                else if (e.keyCode == KeyCode.O) {
+                    SetChoiceSelection(ChoiceSelection.Windows);
+                }
+                else if (e.keyCode == KeyCode.P) {
+                    SetChoiceSelection(ChoiceSelection.Ivy);
+                }
+                else if(e.keyCode == KeyCode.LeftBracket) {
+                    h.RandomizeWindows();
+                }
+
+                if (keyCodes.Contains(e.keyCode)) {
+                    switch (currentChoiceSelection) {
+                        case ChoiceSelection.None:
+                            Set(h.CurrentSelection, h, keyCodes.IndexOf(e.keyCode));
+                            break;
+                        case ChoiceSelection.Color:
+                            SetColor(h, keyCodes.IndexOf(e.keyCode));
+                            break;
+                        case ChoiceSelection.Windows:
+                            if (currentWindowChoice == WindowChoiceSelection.None) {
+                                if (keyCodes.IndexOf(e.keyCode) < System.Enum.GetValues(typeof(WindowChoiceSelection)).Length)
+                                    currentWindowChoice = (WindowChoiceSelection)keyCodes.IndexOf(e.keyCode);
+                            }
+                            else {
+                                if (keyCodes.IndexOf(e.keyCode) == 0)
+                                    currentWindowChoice = WindowChoiceSelection.None;
+                                else {
+                                    if (currentWindowChoice == WindowChoiceSelection.Balcony) {
+                                        h.SetBalcony(keyCodes.IndexOf(e.keyCode));
+                                    }
+                                    else if (currentWindowChoice == WindowChoiceSelection.InsideFloor) {
+                                        h.SetInsideFloor(keyCodes.IndexOf(e.keyCode));
+                                    }
+                                    else if(currentWindowChoice == WindowChoiceSelection.Obstruction) {
+                                        h.SetObstruction(keyCodes.IndexOf(e.keyCode));
+                                    }
+                                }
+                            }
+                            break;
+                        case ChoiceSelection.Ivy:
+                            SetIvy(h, keyCodes.IndexOf(e.keyCode));
+                            break;
+                        default:
+                            break;
+                    }
+                }
+
+                break;
+        }
+
+        
+        Handles.BeginGUI();
+
+        Rect[] nrButtons = new Rect[0];
+        Rect[] nrButtons2 = new Rect[0];
+
+        switch (currentChoiceSelection) {
+            case ChoiceSelection.None:
+                break;
+            case ChoiceSelection.Color:
+                nrButtons = DrawButtons(Screen.width / 2f, Screen.height - 200f, 100f, 50f, 50f, "1 - WHITE", "2 - BEIGE", "3 - PURPLE", "4 - ORANGE");
+                break;
+            case ChoiceSelection.Windows:
+                nrButtons = DrawButtons(Screen.width / 2f, Screen.height - 200f, 100f, 50f, 50f, "1 - BALCONY", "2 - INSIDE FLOOR", "3 - OBSTRUCTION");
+                switch (currentWindowChoice) {
+                    case WindowChoiceSelection.None:
+                        break;
+                    case WindowChoiceSelection.Balcony:
+                        nrButtons2 = DrawButtons(Screen.width / 2f, Screen.height - 300f, 100f, 50f, 50f, "1 - STONE", "2 - PILLARS", "3 - IRON", "0 - RETURN");
+                        break;
+                    case WindowChoiceSelection.InsideFloor:
+                        nrButtons2 = DrawButtons(Screen.width / 2f, Screen.height - 300f, 100f, 50f, 50f, "1 - FIRST FLOOR", "2 - SECOND FLOOR", "3 - THIRD FLOOR", "4 - FOURTH FLOOR", "5 - FIFTH FLOOR", "6 - SIXTH FLOOR", "7 - SEVENTH FLOOR", "8 - EIGTH FLOOR", "9 - OFF", "0 - RETURN");
+                        break;
+                    case WindowChoiceSelection.Obstruction:
+                        nrButtons2 = DrawButtons(Screen.width / 2f, Screen.height - 300f, 100f, 50f, 50f, "1 - LEFT", "2 - RIGHT", "3 - BACK", "0 - RETURN");
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            case ChoiceSelection.Ivy:
+                nrButtons = DrawButtons(Screen.width / 2f, Screen.height - 200f, 100f, 50f, 50f, "1 - OFF", "2 - ON", "3 - ON ALL");
+                break;
+            default:
+                break;
+        }
+
+        
+
+        Rect[] buttons = DrawButtons(Screen.width / 2f, Screen.height - 100f, 100f, 50f, 50f, "U - ADD", "I - REDUCE", "NUM - SET", "C - COLOR", "O - WINDOWS", "P - IVY", "{ - RANDOMIZE WINDOWS");
+        if (Event.current.type == EventType.MouseDown) {
+            bool buttonPressed = false;
+
+            for (int i = 0; i < nrButtons.Length; i++) {
+                if (!nrButtons[i].Contains(Event.current.mousePosition))
+                    continue;
+
+                switch (currentChoiceSelection) {
+                    case ChoiceSelection.None:
+                        Set(h.CurrentSelection, h, i + 1);
+                        break;
+                    case ChoiceSelection.Color:
+                        SetColor(h, i + 1);
+                        break;
+                    case ChoiceSelection.Windows:
+                        if(i + 1 < System.Enum.GetValues(typeof(WindowChoiceSelection)).Length)
+                            currentWindowChoice = (WindowChoiceSelection)i+1;
+                        break;
+                    case ChoiceSelection.Ivy:
+                        SetIvy(h, i + 1);
+                        break;
+                    default:
+                        break;
+                }
+
+                buttonPressed = true;
+                h.ReSelect = true;
+            }
+
+            for (int i = 0; i < nrButtons2.Length; i++) {
+                if (!nrButtons2[i].Contains(Event.current.mousePosition))
+                    continue;
+
+                if (i < nrButtons2.Length - 1) {
+                    switch (currentWindowChoice) {
+                        case WindowChoiceSelection.None:
+                            break;
+                        case WindowChoiceSelection.Balcony:
+                            h.SetBalcony(i + 1);
+                            break;
+                        case WindowChoiceSelection.InsideFloor:
+                            h.SetInsideFloor(i + 1);
+                            break;
+                        case WindowChoiceSelection.Obstruction:
+                            h.SetObstruction(i + 1);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                else
+                    currentWindowChoice = WindowChoiceSelection.None;
+
+                buttonPressed = true;
+                h.ReSelect = true;
+            }
+
+            for (int i = 0; i < buttons.Length; i++) {
+                if (!buttons[i].Contains(Event.current.mousePosition))
+                    continue;
+
+                switch (i) {
+                    case 0:
+                        Add(h.CurrentSelection, h);
+                        break;
+                    case 1:
+                        Reduce(h.CurrentSelection, h);
+                        break;
+                    case 3:
+                        SetChoiceSelection(ChoiceSelection.Color);
+                        break;
+                    case 4:
+                        SetChoiceSelection(ChoiceSelection.Windows);
+                        break;
+                    case 5:
+                        SetChoiceSelection(ChoiceSelection.Ivy);
+                        break;
+                    default:
+                        break;
+                }
+
+                buttonPressed = true;
+                h.ReSelect = true;
+            }
+
+            if (!buttonPressed)
+                h.ReSelect = false;
+        }
+
+        if (h.ReSelect) {
+            Selection.activeGameObject = h.gameObject;
+        }
+
+        Handles.EndGUI();
+    }
+
+    /// <summary>
+    /// Set current selection of house based on mouse position
+    /// </summary>
+    /// <param name="h"></param>
+    private void SetSelection(HouseEditor h) {
+        Vector3 mousePosition = Event.current.mousePosition;
+        Ray ray = HandleUtility.GUIPointToWorldRay(mousePosition);
+        //Debug.Log(mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 1000f)) {
+            //Debug.Log(hit.collider.name);
+            if (hit.collider.gameObject.name == "Roof") {
+                h.CurrentSelection = SelectionType.Roof;
+            }
+            else if (hit.collider.gameObject.name == "RightWall") {
+                h.CurrentSelection = SelectionType.RightWall;
+            }
+            else if (hit.collider.gameObject.name == "BackWall") {
+                h.CurrentSelection = SelectionType.BackWall;
+            }
+            else if (hit.collider.gameObject.name == "FrontWall") {
+                h.CurrentSelection = SelectionType.Front;
+            }
+            else if (hit.collider.gameObject.name == "FrontWallBottom") {
+                h.CurrentSelection = SelectionType.FrontBottom;
+            }
+        }
+    }
+
+    private Rect[] DrawButtons(float xPos, float yPos, float width, float height, float distance, params string[] buttons) {
+        Rect[] r = new Rect[buttons.Length];
+
+        for (int i = 0; i < buttons.Length; i++) {
+            float pos = xPos - (width + distance) * buttons.Length / 2f + (width + distance) * i;
+            Rect rect = new Rect(pos, yPos, width, height);
+            GUI.color = Color.white;
+            GUI.Box(rect, buttons[i]);
+            r[i] = rect;
+        }
+
+        return r;
+    }
+
+    private void SetChoiceSelection(ChoiceSelection selection) {
+        if (currentChoiceSelection != selection)
+            currentChoiceSelection = selection;
+        else
+            currentChoiceSelection = ChoiceSelection.None;
+    }
+
+    private void SetIvy(HouseEditor h, int index) {
+        h.SetIvy(index);
+    }
+
+    private void SetColor(HouseEditor h, int index) {
+        if (index == 1)
+            h.SetColor(0f + 0.01f);
+        else if (index == 2)
+            h.SetColor(1f / 4 + 0.01f);
+        else if (index == 3)
+            h.SetColor(2f / 4 + 0.01f);
+        else if (index == 4)
+            h.SetColor(3f / 4 + 0.01f);
+    }
+
+    private void Add(SelectionType s, HouseEditor h) {
+        switch (s) {
+            case SelectionType.None:
+                break;
+            case SelectionType.Roof:
+                h.AddHeight();
+                break;
+            case SelectionType.RightWall:
+                h.AddWidth();
+                break;
+            case SelectionType.BackWall:
+                h.AddDepth();
+                break;
+            case SelectionType.Front:
+                h.AddWindows();
+                break;
+            case SelectionType.FrontBottom:
+                h.AddWindowsBottom();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void Reduce(SelectionType s, HouseEditor h) {
+        switch (s) {
+            case SelectionType.None:
+                break;
+            case SelectionType.Roof:
+                h.ReduceHeight();
+                break;
+            case SelectionType.RightWall:
+                h.ReduceWidth();
+                break;
+            case SelectionType.BackWall:
+                h.ReduceDepth();
+                break;
+            case SelectionType.Front:
+                h.ReduceWindows();
+                break;
+            case SelectionType.FrontBottom:
+                h.ReduceWindowsBottom();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void Set(SelectionType s, HouseEditor h, int length) {
+        switch (s) {
+            case SelectionType.None:
+                break;
+            case SelectionType.Roof:
+                h.SetHeight(length);
+                break;
+            case SelectionType.RightWall:
+                h.SetWidth(length);
+                break;
+            case SelectionType.BackWall:
+                h.SetDepth(length);
+                break;
+            case SelectionType.Front:
+                h.SetWindows(length);
+                break;
+            case SelectionType.FrontBottom:
+                h.SetWindowsBottom(length);
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+public enum ChoiceSelection
+{
+    None,
+    Color,
+    Windows,
+    Ivy
+}
+
+public enum WindowChoiceSelection
+{
+    None,
+    Balcony,
+    InsideFloor,
+    Obstruction
+}
---
+++ Assets/Scripts/HouseEditor.cs
@@ -0,0 +1,307 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using HoudiniEngineUnity;
+
+[ExecuteInEditMode]
+public class HouseEditor : MonoBehaviour
+{
+    public SelectionType CurrentSelection;
+    public HEU_HoudiniAsset MyHoudiniAsset;
+    public HEU_Parameters MyParameters;
+    public Mesh CubeMesh;
+
+    public Transform Roof;
+    public Transform RightWall;
+    public Transform LeftWall;
+    public Transform BackWall;
+    public Transform FrontWall;
+    public Transform FrontWallBottom;
+
+    public Transform ColliderParent;
+
+    public bool ReSelect = false;
+
+    [SerializeField] private int height;
+    [SerializeField] private int width;
+    [SerializeField] private int depth;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    private void OnDrawGizmosSelected() {
+        if (MyParameters == null)
+            return;
+
+        if (CurrentSelection == SelectionType.Roof) {
+            Gizmos.DrawWireMesh(CubeMesh, Roof.position, transform.rotation, Roof.localScale);
+        }
+        else if (CurrentSelection == SelectionType.RightWall) {
+            Gizmos.DrawWireMesh(CubeMesh, RightWall.position - RightWall.forward * 0.5f, RightWall.rotation, RightWall.localScale);
+        }
+        else if (CurrentSelection == SelectionType.BackWall) {
+            Gizmos.DrawWireMesh(CubeMesh, BackWall.position - BackWall.forward * 0.5f, BackWall.rotation, BackWall.localScale);
+        }
+        else if (CurrentSelection == SelectionType.Front) {
+            Gizmos.DrawWireMesh(CubeMesh, FrontWall.position - FrontWall.forward * 0.25f, FrontWall.rotation, FrontWall.localScale);
+        }
+        else if (CurrentSelection == SelectionType.FrontBottom) {
+            Gizmos.DrawWireMesh(CubeMesh, FrontWallBottom.position - FrontWallBottom.forward * 0.25f, FrontWallBottom.rotation, FrontWallBottom.localScale);
+        }
+    }
+
+    private void SetParameters() {
+        MyHoudiniAsset = GetComponent<HEU_HoudiniAssetRoot>()._houdiniAsset;
+        MyParameters = MyHoudiniAsset.Parameters;
+    }
+
+    public void Cooked(HEU_HoudiniAsset h, bool b, List<GameObject> l) {
+        SetParameters();
+        height = MyParameters.GetParameter("height")._intValues[0];
+        width = MyParameters.GetParameter("width")._intValues[0];
+        depth = MyParameters.GetParameter("depth")._intValues[0];
+
+        //Roof.transform.localPosition = Vector3.up * (height * 2f + 1f) - Vector3.right * depth / 2f - Vector3.forward * width / 2f;
+        Roof.parent = transform;
+        Roof.localPosition = Vector3.up * (height * 2f + 1f) - Vector3.right * width / 2f - Vector3.forward * depth / 2f;
+        Roof.localScale = new Vector3(width, 2f, depth);
+        Roof.localRotation = Quaternion.identity;
+        Roof.parent = ColliderParent;
+
+        RightWall.parent = transform;
+        RightWall.localPosition = Vector3.up * (height * 2f) / 2f - Vector3.right * width - Vector3.forward * depth / 2f;
+        RightWall.localScale = new Vector3(depth, height * 2f, 1f);
+        RightWall.localRotation = Quaternion.Euler(new Vector3(0f, 90f, 0f));
+        RightWall.parent = ColliderParent;
+
+        LeftWall.parent = transform;
+        LeftWall.localPosition = Vector3.up * (height * 2f) / 2f - Vector3.forward * depth / 2f;
+        LeftWall.localScale = new Vector3(depth, height * 2f, 1f);
+        LeftWall.localRotation = Quaternion.Euler(new Vector3(0f, 90f, 0f));
+        LeftWall.parent = ColliderParent;
+
+        BackWall.parent = transform;
+        BackWall.localPosition = Vector3.up * (height * 2f) / 2f - Vector3.right * width / 2f - Vector3.forward * depth;
+        BackWall.localScale = new Vector3(width, height * 2f, 1f);
+        BackWall.localRotation = Quaternion.Euler(new Vector3(0f, 0f, 0f));
+        BackWall.parent = ColliderParent;
+
+        FrontWall.parent = transform;
+        FrontWall.localPosition = Vector3.up * (1f + height * 2f / 2f) - Vector3.right * width / 2f;
+        FrontWall.localScale = new Vector3(width, height * 2f - 2f, 0.5f);
+        FrontWall.localRotation = Quaternion.Euler(new Vector3(0f, 180f, 0f));
+        FrontWall.parent = ColliderParent;
+
+        FrontWallBottom.parent = transform;
+        FrontWallBottom.localPosition = Vector3.up * 1f - Vector3.right * width / 2f;
+        FrontWallBottom.localScale = new Vector3(width, 2f, 0.5f);
+        FrontWallBottom.localRotation = Quaternion.Euler(new Vector3(0f, 180f, 0f));
+        FrontWallBottom.parent = ColliderParent;
+
+        MeshRenderer mr = transform.GetChild(1).GetComponent<MeshRenderer>();
+        int lastEmpty = mr.sharedMaterials.Length;
+        if (mr != null) {
+            for (int i = mr.sharedMaterials.Length - 1; i >= 0; i--) {
+                if (mr.sharedMaterials[i] == null) {
+                    lastEmpty = i;
+                    break;
+                }
+            }
+
+            if (lastEmpty != mr.sharedMaterials.Length) {
+                List<Material> tempMats = new List<Material>(mr.sharedMaterials);
+                for (int i = tempMats.Count - 1; i >= 0; i--) {
+                    if (tempMats[i] == null)
+                        tempMats.RemoveAt(i);
+                }
+                mr.sharedMaterials = tempMats.ToArray();
+            }
+        }
+    }
+
+    public void AddHeight() {
+        MyParameters.GetParameter("height")._intValues = new int[] { height + 1 };
+        MyHoudiniAsset.RequestCook(true, false, false, true);
+    }
+
+    public void ReduceHeight() {
+        if (height - 1 < 1)
+            return;
+        MyParameters.GetParameter("height")._intValues = new int[] { height - 1 };
+        MyHoudiniAsset.RequestCook(true, false, false, true);
+    }
+
+    public void SetHeight(int newHeight) {
+        if (newHeight < 1)
+            return;
+
+        MyParameters.GetParameter("height")._intValues = new int[] { newHeight };
+        MyHoudiniAsset.RequestCook(true, false, false, true);
+    }
+
+    public void AddWidth() {
+        MyParameters.GetParameter("width")._intValues = new int[] { width + 1 };
+        MyHoudiniAsset.RequestCook(true, false, false, true);
+    }
+
+    public void ReduceWidth() {
+        if (width - 1 < 1)
+            return;
+
+        MyParameters.GetParameter("width")._intValues = new int[] { width - 1 };
+        MyHoudiniAsset.RequestCook(true, false, false, true);
+    }
+
+    public void SetWidth(int newWidth) {
+        if (newWidth < 1)
+            return;
+
+        MyParameters.GetParameter("width")._intValues = new int[] { newWidth };
+        MyHoudiniAsset.RequestCook(true, false, false, true);
+    }
+
+    public void AddDepth() {
+        MyParameters.GetParameter("depth")._intValues = new int[] { depth + 1 };
+        MyHoudiniAsset.RequestCook(true, false, false, true);
+    }
+
+    public void ReduceDepth() {
+        if (depth - 1 < 1)
+            return;
+
+        MyParameters.GetParameter("depth")._intValues = new int[] { depth - 1 };
+        MyHoudiniAsset.RequestCook(true, false, false, true);
+    }
+
+    public void SetDepth(int newDepth) {
+        if (newDepth < 1)
+            return;
+
+        MyParameters.GetParameter("depth")._intValues = new int[] { newDepth };
+        MyHoudiniAsset.RequestCook(true, false, false, true);
+    }
+
+    public void AddWindows() {
+        float f = MyParameters.GetParameter("windows")._floatValues[0] - 0.1f;
+
+        if (f < 0f)
+            f = 0.01f;
+
+        MyParameters.GetParameter("windows")._floatValues = new float[] { f };
+        MyHoudiniAsset.RequestCook(true, false, false, true);
+    }
+
+    public void ReduceWindows() {
+        float f = MyParameters.GetParameter("windows")._floatValues[0] + 0.1f;
+
+        if (f > 1f)
+            f = 0.99f;
+
+        MyParameters.GetParameter("windows")._floatValues = new float[] { f };
+        MyHoudiniAsset.RequestCook(true, false, false, true);
+    }
+
+    public void SetWindows(int value) {
+        float f = 1f - (value / 9f);
+
+        MyParameters.GetParameter("windows")._floatValues = new float[] { f };
+        MyHoudiniAsset.RequestCook(true, false, false, true);
+    }
+
+    public void AddWindowsBottom() {
+        float f = MyParameters.GetParameter("windowsBottom")._floatValues[0] - 0.1f;
+
+        if (f < 0f)
+            f = 0.01f;
+
+        MyParameters.GetParameter("windowsBottom")._floatValues = new float[] { f };
+        MyHoudiniAsset.RequestCook(true, false, false, true);
+    }
+
+    public void ReduceWindowsBottom() {
+        float f = MyParameters.GetParameter("windowsBottom")._floatValues[0] + 0.1f;
+
+        if (f > 1f)
+            f = 0.99f;
+
+        MyParameters.GetParameter("windowsBottom")._floatValues = new float[] { f };
+        MyHoudiniAsset.RequestCook(true, false, false, true);
+    }
+
+    public void SetWindowsBottom(int value) {
+        float f = 1f - (value / 9f);
+
+        MyParameters.GetParameter("windowsBottom")._floatValues = new float[] { f };
+        MyHoudiniAsset.RequestCook(true, false, false, true);
+    }
+
+    public void SetColor(float redValue) {
+        Color c = MyParameters.GetParameter("myColor")._color;
+        c.r = redValue;
+        MyParameters.GetParameter("myColor")._color = c;
+        MyHoudiniAsset.RequestCook(true, false, false, true);
+    }
+
+    public void SetIvy(int state) {
+        if (state == 1) {
+            MyParameters.GetParameter("ivyEnabled")._toggle = false;
+        }
+        else if (state == 2) {
+            MyParameters.GetParameter("ivyEnabled")._toggle = true;
+            MyParameters.GetParameter("ivyNO")._toggle = false;
+        }
+        else if (state == 3) {
+            MyParameters.GetParameter("ivyEnabled")._toggle = true;
+            MyParameters.GetParameter("ivyNO")._toggle = true;
+        }
+
+        MyHoudiniAsset.RequestCook(true, false, false, true);
+    }
+
+    public void SetBalcony(int state) {
+        if (state - 1 >= 0 && state - 1 <= 2) {
+            MyParameters.GetParameter("balconyType")._intValues = new int[] { state - 1 };
+            MyHoudiniAsset.RequestCook(true, false, false, true);
+        }
+    }
+
+    public void SetInsideFloor(int floor) {
+        if (floor == 9) floor = 0;
+        MyParameters.GetParameter("insideFloor")._intValues = new int[] { floor };
+        MyHoudiniAsset.RequestCook(true, false, false, true);
+    }
+
+    public void SetObstruction(int wall) {
+        if(wall == 1)
+            MyParameters.GetParameter("rightObstructed")._toggle = !MyParameters.GetParameter("rightObstructed")._toggle;
+        else if(wall == 2)
+            MyParameters.GetParameter("leftObstructed")._toggle = !MyParameters.GetParameter("leftObstructed")._toggle;
+        else if (wall == 3)
+            MyParameters.GetParameter("backObstructed")._toggle = !MyParameters.GetParameter("backObstructed")._toggle;
+        MyHoudiniAsset.RequestCook(true, false, false, true);
+    }
+
+    public void RandomizeWindows() {
+        float f = Random.value;
+        MyParameters.GetParameter("windowSeed")._floatValues = new float[] { f };
+        MyHoudiniAsset.RequestCook(true, false, false, true);
+    }
+}
+
+public enum SelectionType
+{
+    None,
+    Roof,
+    RightWall,
+    BackWall,
+    Front,
+    FrontBottom
+}
